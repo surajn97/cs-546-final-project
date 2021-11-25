73357649019991fa3d12be4e66c528db41298007
Suraj Nair <suraj2497@gmail.com>
2021-11-25 16:58:47 -0500

    Trial

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const configRoutes = require("./routes");
 app.use(express.json());
 
 configRoutes(app);
-
+//trial
 app.listen(3000, () => {
   console.log("We've now got a server!");
   console.log("Your routes will be running on http://localhost:3000");
