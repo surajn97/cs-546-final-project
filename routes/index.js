d3a550b663853abf77cbeb918787f70258ee59b6
zjzhao23 <82527939+zjzhao23@users.noreply.github.com>
2021-12-03 10:22:20 -0500

    Add files via upload

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,18 @@
 const restaurantRoutes = require("./recipes");
 const reviewRoutes = require("./reviews");
+const userRoutes = require('./users');
+const privateRoutes = require('./private');
+
 
 const constructorMethod = app => {
   // app.use("/restaurants", restaurantRoutes);
   // app.use("/reviews", reviewRoutes);
   // app.use("/users", userRoutes);
   // app.use("/comments", commentRoutes);
+  // app.use('/private', privateRoutes);
+  // app.use('/', userRoutes); // If the user is authenticated, it will redirect to /private.
+  // app.use('/private', privateRoutes);
+
 
   app.use("*", (req, res) => {
     res.status(404).json({ error: "Page not found" });
